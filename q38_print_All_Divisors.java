e7173175fbc00837187a39fec73a01f864c6739d
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-01 20:42:32 +0530

    q38

---
+++ q38_print_All_Divisors.java
@@ -0,0 +1,11 @@
+public class q38_print_All_Divisors {
+
+    public static void main(String[] args) {
+        int n=12;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
