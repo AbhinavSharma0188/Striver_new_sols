b587c14b4c91970f410ddb7121edffa9819e9119
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-01 16:25:32 +0530

    q35

---
+++ q35.java
@@ -0,0 +1,20 @@
+public class q35 {
+    public static void main(String[] args) {
+        int n=121;
+        int original=n;
+        
+        int rev=0;
+        while(n!=0){
+            rev=rev*10+n%10;
+            n=n/10;
+
+        }
+        if(rev==original){
+            System.out.println("the number is palindrome ");
+        }
+        else{
+            System.out.println("Not palindrome");
+        }
+    }
+
+}
