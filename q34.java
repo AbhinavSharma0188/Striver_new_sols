39c008f8c71f8acf90a383fb7d49119650e1f80c
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-01 16:16:15 +0530

    q34

---
+++ q34.java
@@ -0,0 +1,13 @@
+public class q34 {
+    public static void main(String[] args) {
+        int n=12334;
+        int rev=0;
+       while(n!=0){
+         rev=rev*10+n%10;
+        n=n/10;
+
+       }
+       System.out.println(rev);
+    }
+    
+}
