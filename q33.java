cd2adead1f673cac0026981d4b540ae078c6d074
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-01 16:06:23 +0530

    q33

---
+++ q33.java
@@ -0,0 +1,16 @@
+// Count Digits 
+
+class q33{
+    public static void main(String[] args) {
+        int n=12345;
+        int count=0;
+
+        while(n!=0){
+             n=n/10;
+            count=count+1;
+
+
+        }
+        System.out.println(count);
+    }
+}
