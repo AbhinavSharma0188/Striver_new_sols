bcd8acb72dcd270f04ee00a86d0bf544017b204c
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-11 14:41:11 +0530

    q57

---
+++ q57_Quick_Sort.java
@@ -0,0 +1,37 @@
+public class q57_Quick_Sort{
+    public static void main(String[] args) {
+        int arr[]={8,7,3,1,4};
+        Sort(arr,0,arr.length-1);
+        for(int i=0;i<=arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    public static void Sort(int arr[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int idx=partition(arr,si,ei);
+        Sort(arr,si,idx-1);
+        Sort(arr,idx+1,ei);
+    }
+     public static int partition(int arr[],int si,int ei){
+        int item=arr[ei];
+        int idx=si;
+        for(int i=si;i<ei;i++){
+            if(arr[i]<=item){
+                int temp=arr[i];
+                arr[i]=arr[idx];
+                arr[idx]=temp;
+                idx++;
+            }
+
+
+        }
+        int temp=arr[ei];
+        arr[ei]=arr[idx];
+        arr[idx]=temp;
+     return idx;
+    }
+    
+}
