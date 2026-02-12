91eba9c5094e8384240e2f16130e9584798a9f51
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-12 19:52:54 +0530

    q59

--- q59_Second_LargestElementInanArray.java
+++ q59_Second_LargestElementInanArray.java
@@ -1,17 +1,20 @@
 public class q59_Second_LargestElementInanArray {
     public static void main(String[] args) {
-
-        int arr[] = {4,3,2};
-
-        int maxVal = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
-
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] > maxVal){
-                secondLargest = maxVal;
-                maxVal = arr[i];
+        int arr[]={4,3,2};
+        int maxVal=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]>maxVal){
+                secondLargest=maxVal;
+                maxVal=arr[i];
             }
-            else if(arr[i] > secondLargest && arr[i] != maxVal){
-                secondLargest = arr[i];
+            else if(arr[i]>secondLargest&&arr[i]!=maxVal){
+                secondLargest=arr[i];
             }
         }
+
+        System.out.println(secondLargest);
+        
+    }
+    
+}
