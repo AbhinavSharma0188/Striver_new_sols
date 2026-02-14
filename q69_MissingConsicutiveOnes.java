e5c73ad2b7f1bd181d3ed6baaeb16f2030c1c60e
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-14 22:38:14 +0530

    q69

---
+++ q69_MissingConsicutiveOnes.java
@@ -0,0 +1,24 @@
+public class q69_MissingConsicutiveOnes {
+    public static void main(String[] args) {
+        int arr[]={1,1,0,1,1,1};
+        int cons=0;
+        int maxCons=0;
+        for(int i=0;i<=arr.length-1;i++){
+            
+            if(arr[i]!=1){
+                cons=0;
+            }
+            else{
+                cons=cons+1;
+            }
+            if(cons>maxCons){
+                maxCons=cons;
+            }
+
+
+
+        }
+        System.out.println(maxCons);
+    }
+    
+}
