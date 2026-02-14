58036606456a9326c3ee5bd616f5dfacc55db0c9
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-14 23:03:34 +0530

    q70

---
+++ q70_SingleNumber.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class q70_SingleNumber {
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int arr[] = {2,2,1};
+
+        for(int i = 0; i < arr.length; i++){
+            if(map.containsKey(arr[i])){
+                map.remove(arr[i]);
+            } else {
+                map.put(arr[i], i);
+            }
+        }
+
+        // Print only the single number (not in array format)
+        for(int key : map.keySet()){
+            System.out.println(key);
+        }
+    }
+}
