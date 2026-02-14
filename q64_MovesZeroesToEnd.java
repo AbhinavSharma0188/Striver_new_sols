64ac166fbb74a26d4a892658e57bfcac4354ceed
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-14 21:43:14 +0530

    q64

---
+++ q64_MovesZeroesToEnd.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class q64_MovesZeroesToEnd {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,0,2,0,2,0,2,0};
+        int j=0;
+        int result[]=new int[arr.length];
+    
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]!=0){
+                result[j]=arr[i];
+                j++;
+            }
+
+            
+           
+        }
+        while(j<=arr.length-1){
+            result[j]=0;
+            j++;
+        }
+        for(int a=0;a<=result.length-1;a++){
+            System.out.print(result[a]+" ");
+        }
+
+       
+        
+    }
+    
+}
