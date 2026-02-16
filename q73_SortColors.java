7a4e7dd002cf7e8f64f234f920efbba4c4aae128
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-16 20:31:11 +0530

    q73

---
+++ q73_SortColors.java
@@ -0,0 +1,32 @@
+public class q73_SortColors {
+    public static void main(String[] args) {
+        int arr[]={2,0,2,1,1,0,1,0,1,0};
+        int start=0;
+        int mid=0;
+        int end=arr.length-1;
+        while(mid<=end){
+            if(arr[mid]==0){
+                int temp=arr[start];
+                arr[start]=arr[mid];
+                arr[mid]=temp;
+                start++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+
+            }
+            else{
+                int temp=arr[mid];
+                arr[mid]=arr[end];
+                arr[end]=temp;
+                
+                end--;
+            }
+        }
+      for(int i=0;i<=arr.length-1;i++){
+        System.out.print(arr[i]+" ");
+      }
+    }
+    
+}
