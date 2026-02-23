b648d41fbbace199a9c40e5289f89561488b8112
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-23 14:31:28 +0530

    q83

---
+++ q83_RotateMatrixby90Degree.java
@@ -0,0 +1,30 @@
+public class q83_RotateMatrixby90Degree {
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3},{3,2,4},{5,3,2}};
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+    }
+    
+}
