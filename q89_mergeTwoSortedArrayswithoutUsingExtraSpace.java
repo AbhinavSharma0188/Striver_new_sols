4b8280146b8dbc9f8f001b15bb5842269b448eb1
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-24 19:39:16 +0530

    q89

---
+++ q89_mergeTwoSortedArrayswithoutUsingExtraSpace.java
@@ -0,0 +1,22 @@
+// class Solution {
+//     public void mergeArrays(int arr1[], int arr2[]) {
+        
+//         // code here
+//         int i=arr1.length-1;
+//         int j=0;
+//         while(i>=0&&j<=arr2.length-1){
+//             if(arr1[i]>arr2[j]){
+//                 int temp=arr1[i];
+//                 arr1[i]=arr2[j];
+//                 arr2[j]=temp;
+//                 i--;
+//                 j++;
+//             }
+//             else{break;}
+//         }
+//         Arrays.sort(arr1);
+//         Arrays.sort(arr2);
+        
+        
+//     }
+// }
