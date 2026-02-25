6b3c48fda8325b7d38999b1b6c2d29660785cd49
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-25 16:23:42 +0530

    /

--- q89_mergeTwoSortedArrayswithoutUsingExtraSpace.java
+++ q89_mergeTwoSortedArrayswithoutUsingExtraSpace.java
@@ -8,11 +8,11 @@
 //             if(arr1[i]>arr2[j]){
 //                 int temp=arr1[i];
 //                 arr1[i]=arr2[j];
-//                 arr2[j]=temp;
+//                 arr2[j]=temp; 
 //                 i--;
 //                 j++;
 //             }
-//             else{break;}
+//             else{break;;}
 //         }
 //         Arrays.sort(arr1);
 //         Arrays.sort(arr2);
