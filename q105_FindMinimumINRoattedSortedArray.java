689158d89d48c129aa1eccb5001f73d09b785916
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-02-28 21:37:01 +0530

    q105

---
+++ q105_FindMinimumINRoattedSortedArray.java
@@ -0,0 +1,18 @@
+public class q105_FindMinimumINRoattedSortedArray {
+    public static void main(String[] args) {
+        int arr[]={4,5,6,7,0,1,2};
+        int left=0;
+        int right=arr.length-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]>arr[right]){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        System.out.println(arr[left]);
+    }
+    
+}
