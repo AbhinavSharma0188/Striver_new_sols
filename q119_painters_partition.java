1b0387880b152480e54e9d0c8d17c953b26ce4c0
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-03-05 14:03:50 +0530

    q119

---
+++ q119_painters_partition.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minTime(int[] arr, int k) {
+        // code here
+        int low=0;
+        int high=0;
+        int ans=0;
+        if(k>arr.length) {
+            return -1;
+        }
+        for(int i=0;i<=arr.length-1;i++){
+            low=Math.max(low,arr[i]);
+            high=high+arr[i];
+        }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isItpossible(arr,mid,k)){
+                ans=mid;
+                high=mid-1;
+                
+                
+            }
+            low=mid+1;
+            
+        }
+        return ans;
+        
+    }
+    public static boolean isItpossible(int arr[],int mid,int k){
+        int pcount=1;
+        int count=0;
+        for(int i=0;i<=arr.length-1;i++){
+        if(arr[i]+count<=mid){
+                count+=arr[i];
+        }
+        else{
+            pcount++;
+            count=arr[i];
+        }
+        if(pcount>k){
+            return false;
+        }
+        }
+        return true;
+    }
+}
