43589b3e07c1ba6c57688bc445955a14cfc9201f
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-03-07 15:03:43 +0530

    q122

---
+++ q122_SearchInASortedArray.java
@@ -0,0 +1,19 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] arr, int item) {
+//         int i = 0;
+//         int j = arr[0].length - 1;
+
+//         while(i < arr.length && j >= 0){
+//             if(arr[i][j] == item){
+//                 return true;
+//             }
+//             else if(arr[i][j] > item){
+//                 j--;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         return false;
+//     }
+// }
