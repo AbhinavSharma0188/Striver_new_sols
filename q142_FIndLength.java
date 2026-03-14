328b75631171f7da14cd31066ae83270648f031e
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-03-14 16:32:33 +0530

    q142

---
+++ q142_FIndLength.java
@@ -0,0 +1,59 @@
+public class q142_FIndLength {
+ 
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    // Function to find length
+    public int findLength() {
+        int count = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        return count;
+    }
+
+    // Function to insert at end
+    public void addLast(int val) {
+        Node newNode = new Node(val);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+
+        System.out.println("Length of Linked List: " + list.findLength());
+    }
+}
+    
+
