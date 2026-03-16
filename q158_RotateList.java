373cc029cc3878726f1d70d50bc5dd90d277a30f
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-03-16 18:35:03 +0530

    q158

---
+++ q158_RotateList.java
@@ -0,0 +1,33 @@
+// class Solution {
+//     public ListNode rotateRight(ListNode head, int k) {
+
+//         if(head == null || head.next == null || k == 0){
+//             return head;
+//         }
+
+//         ListNode temp = head;
+//         int count = 1;
+
+//         while(temp.next != null){
+//             temp = temp.next;
+//             count++;
+//         }
+
+//         temp.next = head;   // make circular
+
+//         k = k % count;
+
+//         int jump = count - k;
+
+//         temp = head;
+
+//         for(int i = 1; i < jump; i++){
+//             temp = temp.next;
+//         }
+
+//         ListNode newHead = temp.next;
+//         temp.next = null;
+
+//         return newHead;
+//     }
+// }
