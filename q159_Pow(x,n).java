e97b554920020d5cb55b73af2f355dbb508c72d6
Abhinav Sharma <abhinavsharma3614@gmail.com>
2026-03-17 15:04:11 +0530

    q159

---
+++ q159_Pow(x,n).java
@@ -0,0 +1,17 @@
+// class Solution {
+//     public double myPow(double x, long n) {
+//         if (n == 0) return 1;
+
+//         if (n < 0) {
+//             return myPow(1 / x, -n);
+//         }
+
+//         double half = myPow(x * x, n / 2);
+
+//         if (n % 2 == 0) {
+//             return half;
+//         } else {
+//             return x * half;
+//         }
+//     }
+// }
